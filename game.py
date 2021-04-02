7708199fa0112d031f589f356050df9010423f84
Velavee <59717974+Velavee@users.noreply.github.com>
2021-04-02 15:04:24 -0700

    moved an element in an array

--- game.py
+++ game.py
@@ -1,4 +1,24 @@
 import pygame
 from pygame.locals import *
 
-print('Hi Max')
+background = [1, 1, 2, 2, 2, 1]
+screen = [0]*6
+for i in range(6):
+    screen[i] = background[i]
+print(screen)
+playerpos = 3
+screen[playerpos] = 8
+print(screen)
+
+screen[playerpos] = background[playerpos]
+playerpos = playerpos + 1
+screen[playerpos] = 8
+print(screen)
+
+pygame.init()
+pygame.display.init()
+
+print(pygame.display.get_init)
+
+background = pygame.image.load('terrain.png').convert()
+screen.blit(background, (0, 0))
